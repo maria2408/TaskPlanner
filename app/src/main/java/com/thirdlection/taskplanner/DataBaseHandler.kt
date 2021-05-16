d8b38b58241c54fb2c7b8706c4bac7c64d38715b
Maria <marianedoshivina@gmail.com>
2021-05-16 22:45:21 +0300

    исправлены ошибки

--- app/src/main/java/com/thirdlection/taskplanner/DataBaseHandler.kt
+++ app/src/main/java/com/thirdlection/taskplanner/DataBaseHandler.kt
@@ -6,38 +6,40 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.widget.Toast
 
-val DatabaseName: String = "MyTasks"
-val TableName: String = "Tasks"
-val ColId: String = "id"
-val ColName: String = "name"
-val ColDescrip: String = "description"
-val ColDeadlineDate: String = "Deadline_date"
-val ColDeadlineTime: String = "Deadline_time"
-val ColDurStartD: String = "Duration_start_date"
-val ColDurEndD: String = "Duration_end_date"
-val ColDurStartT: String = "Duration_start_time"
-val ColDurEndT: String = "Duration_end_time"
-val ColImp: String = "Importance"
+object Constants {
+    const val DatabaseName: String = "MyTasks"
+    const val TableName: String = "Tasks"
+    const val ColId: String = "id"
+    const val ColName: String = "name"
+    const val ColDescrip: String = "description"
+    const val ColDeadlineDate: String = "Deadline_date"
+    const val ColDeadlineTime: String = "Deadline_time"
+    const val ColDurStartD: String = "Duration_start_date"
+    const val ColDurEndD: String = "Duration_end_date"
+    const val ColDurStartT: String = "Duration_start_time"
+    const val ColDurEndT: String = "Duration_end_time"
+    const val ColImp: String = "Importance"
+}
 
 class DataBaseHandler(private var context: Context?) :
     SQLiteOpenHelper(
         context,
-        DatabaseName,
+        Constants.DatabaseName,
         null,
         1
     ) {
     override fun onCreate(db: SQLiteDatabase?) {
-        val createTable = "CREATE TABLE " + TableName + " (" +
-            ColId + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-            ColName + " VARCHAR(256)," +
-            ColDescrip + " VARCHAR(256)," +
-            ColDeadlineDate + " VARCHAR(256)," +
-            ColDeadlineTime + " VARCHAR(256)," +
-            ColDurStartD + " VARCHAR(256)," +
-            ColDurEndD + " VARCHAR(256)," +
-            ColDurStartT + " VARCHAR(256)," +
-            ColDurEndT + " VARCHAR(256)," +
-            ColImp + " INTEGER" +
+        val createTable = "CREATE TABLE " + Constants.TableName + " (" +
+            Constants.ColId + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            Constants.ColName + " VARCHAR(256)," +
+            Constants.ColDescrip + " VARCHAR(256)," +
+            Constants.ColDeadlineDate + " VARCHAR(256)," +
+            Constants.ColDeadlineTime + " VARCHAR(256)," +
+            Constants.ColDurStartD + " VARCHAR(256)," +
+            Constants.ColDurEndD + " VARCHAR(256)," +
+            Constants.ColDurStartT + " VARCHAR(256)," +
+            Constants.ColDurEndT + " VARCHAR(256)," +
+            Constants.ColImp + " INTEGER" +
             ") "
         db?.execSQL(createTable)
     }
@@ -49,16 +51,16 @@ class DataBaseHandler(private var context: Context?) :
     fun insertData(task: Task) {
         val db = this.writableDatabase
         val cv = ContentValues()
-        cv.put(ColName, task.name)
-        cv.put(ColDescrip, task.desc)
-        cv.put(ColDeadlineDate, task.deadlineDate)
-        cv.put(ColDeadlineTime, task.deadlineTime)
-        cv.put(ColDurStartD, task.durStartDate)
-        cv.put(ColDurEndD, task.durEndDate)
-        cv.put(ColDurStartT, task.durStartTime)
-        cv.put(ColDurEndT, task.durEndTime)
-        cv.put(ColImp, task.importance)
-        val result = db.insert(TableName, null, cv)
+        cv.put(Constants.ColName, task.name)
+        cv.put(Constants.ColDescrip, task.desc)
+        cv.put(Constants.ColDeadlineDate, task.deadlineDate)
+        cv.put(Constants.ColDeadlineTime, task.deadlineTime)
+        cv.put(Constants.ColDurStartD, task.durStartDate)
+        cv.put(Constants.ColDurEndD, task.durEndDate)
+        cv.put(Constants.ColDurStartT, task.durStartTime)
+        cv.put(Constants.ColDurEndT, task.durEndTime)
+        cv.put(Constants.ColImp, task.importance)
+        val result = db.insert(Constants.TableName, null, cv)
         if (result == -1.toLong())
             Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
         else
